be0f1dab403f52bf57a27e1bf9ef76a4b900218f
HermesDE <valentinwagner2000@outlook.de>
2022-10-22 17:20:33 +0200

    added informations

--- components/Frontpage/Informations.js
+++ components/Frontpage/Informations.js
@@ -1,3 +1,20 @@
+import { Title, Text, Grid, Container } from "@mantine/core";
+
 export default function Informations() {
-  return <h1>News and Informations</h1>;
+  return (
+    <>
+      <Container fluid mt={50}>
+        <Grid>
+          <Title order={1}>News and Informations</Title>
+        </Grid>
+        <Grid mt={10}>
+          <Text weight={400}>
+            Attention! This application is currently under rapid development and
+            is still in an early testing phase. It is not guaranteed that all
+            user data will be preserved.
+          </Text>
+        </Grid>
+      </Container>
+    </>
+  );
 }
