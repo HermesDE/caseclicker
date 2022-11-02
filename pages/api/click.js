dd701523802e48472e91edfc9ef429ab18b43ed9
HermesDE <valentinwagner2000@outlook.de>
2022-11-02 23:15:54 +0100

    remove rate limiter

--- pages/api/click.js
+++ pages/api/click.js
@@ -1,23 +1,8 @@
 import connectDB from "../../lib/database/connectMongoDb";
 import UserStat from "../../lib/database/schemas/userStat";
-import { authOptions } from "../api/auth/[...nextauth]";
-import { unstable_getServerSession } from "next-auth/next";
-import { RateLimiterMemory } from "rate-limiter-flexible";
 import { getToken } from "next-auth/jwt";
 
-const opts = {
-  points: 20,
-  duration: 1,
-};
-const rateLimiter = new RateLimiterMemory(opts);
-
 async function handler(req, res) {
-  try {
-    await rateLimiter.consume(req.connection.remoteAddress);
-  } catch (ex) {
-    return res.status(429).json({ error: "Rate limit exeeded" });
-  }
-
   const token = await getToken({ req });
   const userId = token.id;
 
