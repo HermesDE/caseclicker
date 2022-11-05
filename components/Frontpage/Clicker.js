dbf3c507c1f53256d961c00e6fa48704889e535e
HermesDE <valentinwagner2000@outlook.de>
2022-11-05 19:08:38 +0100

    clicker websocket

--- components/Frontpage/Clicker.js
+++ components/Frontpage/Clicker.js
@@ -14,6 +14,10 @@ import useSound from "use-sound";
 import { motion } from "framer-motion";
 import { showNotification } from "@mantine/notifications";
 import DollarIcon from "../icons/DollarIcon";
+import { getSession } from "next-auth/react";
+import { io } from "socket.io-client";
+
+let socket;
 
 export default function Clicker({
   money,
@@ -21,7 +25,43 @@ export default function Clicker({
   moneyPerClick,
   setMoneyPerClick,
 }) {
+  const [connected, setConnected] = useState(false);
+  const [userSession, setUserSession] = useState(null);
   const [play] = useSound("/sounds/moneyClick.mp3", { volume: 0.05 });
+
+  useEffect(() => {
+    const initConnection = async () => {
+      const session = await getSession();
+      setUserSession(session);
+      socket = io("http://localhost:3001", { auth: session });
+      socket.on("connect_error", (err) => {
+        console.log(err.message);
+      });
+      socket.on("connect", () => {
+        setConnected(true);
+        socket.on("click", (data) => {
+          setMoney(data.money);
+          setMoneyPerClick(data.moneyPerClick);
+        });
+      });
+      socket.on("blocked", (data) => {
+        showNotification({
+          title: "You are blocked",
+          message: `You are blocked because you are clicking too fast.\nPlease wait another ${data}ms until your next click`,
+          color: "red",
+        });
+      });
+    };
+    initConnection();
+
+    if (socket) {
+      return () => {
+        socket.off("connect");
+        socket.off("connect_error");
+        socket.off("click");
+      };
+    }
+  }, []);
 
   return (
     <Container>
@@ -45,7 +85,10 @@ export default function Clicker({
                   color="#CCCC00"
                   onClick={async () => {
                     play();
-                    const response = await fetch("/api/click", {
+
+                    socket.emit("click");
+
+                    /* const response = await fetch("/api/click", {
                       method: "POST",
                     });
                     if (!response.ok) {
@@ -61,7 +104,7 @@ export default function Clicker({
                     }
                     const userStat = await response.json();
                     setMoney(userStat.money);
-                    setMoneyPerClick(userStat.moneyPerClick);
+                    setMoneyPerClick(userStat.moneyPerClick); */
                   }}
                 />
               </UnstyledButton>
