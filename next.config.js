225bf22bb4fe9e732594e9c69efad511381d0242
HermesDE <valentinwagner2000@outlook.de>
2022-11-28 18:22:03 +0100

    service worker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
-  dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
   scope: "/app",
