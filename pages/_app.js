09bff1f647cc36a489228bcdb39767e6954e9db5
Valentin Wagner <v.wagner@lzw.mlu.sachsen-anhalt.de>
2023-01-10 14:02:14 +0100

    remove google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ import {
 import { ModalsProvider } from "@mantine/modals";
 import { useState, useEffect } from "react";
 import Head from "next/head";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const [money, setMoney] = useState(0);
@@ -91,7 +90,6 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
         >
           <NotificationsProvider>
             <ModalsProvider>
-              <GoogleAnalytics trackPageViews />
               <Component
                 toggleMoneyUpdate={toggleMoneyUpdate}
                 money={money}
